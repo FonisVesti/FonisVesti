fb3b596536674451b01633b3b1c178e54e92138a
Dusan <dulerad94@gmail.com>
2016-03-24 02:17:53 +0100

    Ubaceno sve sto je do sad uradjeno

---
+++ app/src/main/java/com/vesti/fonis/fonisvesti/Slika.java
@@ -0,0 +1,15 @@
+package com.vesti.fonis.fonisvesti;
+
+/**
+ * Created by Dusan on 21.3.2016..
+ */
+public class Slika {
+    String src;
+    String naslov;
+
+    public Slika(String src,String naslov){
+        this.src=src;
+        this.naslov=naslov;
+    }
+
+}
