62c992063a380a16e614cd9af7f5000f04c243c6
Radovan Ristovic <radovanr995@gmail.com>
2016-04-18 02:56:40 +0200

    UI changes, search interface added, appbar with submenu added

--- app/src/main/java/com/vesti/fonis/fonisvesti/MainActivity.java
+++ app/src/main/java/com/vesti/fonis/fonisvesti/MainActivity.java
@@ -1,17 +1,24 @@
 package com.vesti.fonis.fonisvesti;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends ActionBarActivity {
 
-    private static ImageButton ibtn;
+    private static RelativeLayout mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +33,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void onClickIbtnVestiListener() {
-        ibtn = (ImageButton) findViewById(R.id.ibtnVesti);
-        ibtn.setOnClickListener(
+        mButton = (RelativeLayout) findViewById(R.id.rlVesti);
+        mButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -39,8 +46,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void onClickIbtnONamaListener() {
-        ibtn = (ImageButton) findViewById(R.id.ibtnONama);
-        ibtn.setOnClickListener(
+        mButton = (RelativeLayout) findViewById(R.id.rlOnama);
+        mButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -52,8 +59,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void onClickIbtnProjektiListener() {
-        ibtn = (ImageButton) findViewById(R.id.ibtnProjekti);
-        ibtn.setOnClickListener(
+        mButton = (RelativeLayout) findViewById(R.id.rlProjekti);
+        mButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -65,8 +72,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void onClickIbtnMSDNAAListener() {
-        ibtn = (ImageButton) findViewById(R.id.ibtnMSDNAA);
-        ibtn.setOnClickListener(
+        mButton = (RelativeLayout) findViewById(R.id.rlMsdnaa);
+        mButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -88,4 +95,42 @@ public class MainActivity extends ActionBarActivity {
         });
     }
     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        // TODO - add search dialog if there's no space for searchview
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id){
+            case R.id.action_settings:
+                Intent i = new Intent(this, PreferencesActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+
+
+    }
 }
--- app/src/main/java/com/vesti/fonis/fonisvesti/VestiActivity.java
+++ app/src/main/java/com/vesti/fonis/fonisvesti/VestiActivity.java
@@ -1,18 +1,33 @@
 package com.vesti.fonis.fonisvesti;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
 
 /**
  * Created by Dusan on 24.3.2016..
  */
 public class VestiActivity extends BaseActivity{
-    int brojStrane;
+    private int brojStrane;
+    private String[] searchResults;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vesti);
         brojStrane=0;
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            searchResults = searchNews(query);
+        }
+
+        // TODO - create custom adapter
+        //ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.activity_list_item,searchResults);
+       //setListAdapter(arrayAdapter);
 
     }
 
@@ -21,6 +36,15 @@ public class VestiActivity extends BaseActivity{
         super.onStart();
         brojStrane=1;
         Vesti.svuciVesti(brojStrane);
+    }
+
+    // TODO - implement searchNews method
+    /**
+     * @param query text that is searched for
+     * @return search results
+     */
+    private String[] searchNews(String query) {
+        return null;
     }
 
 }
